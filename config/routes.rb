a626026655dd26e18ccf8770ebb221f45b99e51b
Fujimura Daisuke <me@fujimuradaisuke.com>
2011-08-31 14:25:18 +0900

    top#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FbGraphRailsExample::Application.routes.draw do
+  get "top/index"
+
   resources :users
 
   resources :posts
@@ -52,7 +54,7 @@ FbGraphRailsExample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'top#index'
 
   # See how all your routes lay out with "rake routes"
 
