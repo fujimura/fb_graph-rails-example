e07d88a58ee39b32b2cb5b1226f51c63b2d7332b
Fujimura Daisuke <me@fujimuradaisuke.com>
2011-08-31 13:56:37 +0900

    Post, User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FbGraphRailsExample::Application.routes.draw do
+  resources :users
+
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
