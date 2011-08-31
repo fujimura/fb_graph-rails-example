3c609e130bbc129aa5e0eb5c7baa67f8296e2e93
Fujimura Daisuke <me@fujimuradaisuke.com>
2011-08-31 13:54:31 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbGraphRailsExample::Application.config.secret_token = 'fd4fcab8326d9bf56d339e9b6a22ee42b496ed37ab170b8b5d71450716d3b0ba00d269487be62f647a302ca2420aed5f35c80eaa9dbba2bea0555aa300555814'
